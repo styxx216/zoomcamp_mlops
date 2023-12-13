16e7ee30b0f18bcac43485e0b9ffd8eddc143d09
styxx216 <ko4etova.elena.n@gmail.com>
2023-12-13 20:29:12 +0400

    0

--- zoomcamp_mlops_homework_5/task_3.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-print('start')
-app = Flask('churn-app') # give an identity to your web service
-@app.route('/ping',methods=['GET'])
-def ping():
-    return 'PONG'
-
-app.run(debug=True, host='localhost', port=9696)
--- zoomcamp_mlops_homework_5/task_4_test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://localhost:9696/predict'
-client = {"job": "unknown", "duration": 270, "poutcome": "failure"}
-response = requests.post(url, json=client).json()
-
-print(response)
